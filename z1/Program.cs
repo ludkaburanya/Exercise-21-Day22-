1b5020f5397972b364b982d7100fae6cc2b967de
HP <HP@VLADIKPROGRAMME>
2023-03-20 20:46:35 +0300

    Добавьте файлы проекта.

---
+++ z1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace z1
+{
+    class Program
+    {
+        static void Main()
+        {
+            z1 classLoner = z1.GetInstance();
+            classLoner.Name = "classLoner1";
+            Console.WriteLine(classLoner.Name);
+            z1 classLoner2 = z1.GetInstance();
+            classLoner2.Name = "classLoner2";
+            Console.WriteLine(classLoner2.Name);
+            Console.WriteLine(classLoner.Name);
+        }
+    }
+}
---
+++ z1/z1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z1
+{
+    internal class z1
+    {
+        private static z1 uniqueInstance;
+        public string Name { get; set; }
+        private z1()
+        {
+
+        }
+        public static z1 GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new z1();
+            }
+            return uniqueInstance;
+        }
+    }
+}
